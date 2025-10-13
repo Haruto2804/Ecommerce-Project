80f8a5a9a1984d4fbb2e226e86069738a0b9d444
= <=>
2025-10-13 19:45:52 +0700

    Move checkout page into React

--- e-commerce-project/src/App.jsx
+++ e-commerce-project/src/App.jsx
@@ -1,15 +1,16 @@
 
-import { Routes,Route } from 'react-router'
-import {HomePage} from './pages/HomePage'
+import { Routes, Route } from 'react-router'
+import { HomePage } from './pages/HomePage'
+import {CheckoutPage} from './pages/CheckoutPages'
 import './App.css'
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route index element = {<HomePage />} />
-      <Route path = "checkout" element = {<div>Test checkout page</div>} />
-    </Routes>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="checkout" element={<CheckoutPage />} />
+    </Routes >
     
     </>
   )
