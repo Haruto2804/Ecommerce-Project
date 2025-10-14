b458477e61dea8dadc6d4d63a2c5591bca48eae3
= <=>
2025-10-14 07:55:50 +0700

    move tracking into react

--- e-commerce-project/src/App.jsx
+++ e-commerce-project/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import {CheckoutPage} from './pages/checkout/CheckoutPages'
 import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="checkout" element={<CheckoutPage />} />
         <Route path="order" element={<OrdersPage />} />
+        <Route path="tracking" element={<TrackingPage />} />
                 
     </Routes >
     
--- e-commerce-project/src/pages/HomePage.jsx
+++ e-commerce-project/src/pages/HomePage.jsx
@@ -6,7 +6,9 @@ import './HomePage.css'
 export function HomePage() {
   return (
     <>
+    
         <title>Ecommerce Project</title>
+         <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
       <Header />
       
       <div className="home-page">
