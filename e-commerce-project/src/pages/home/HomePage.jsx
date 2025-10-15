0cca970dddaca8c6d9a8d21b990568d498db8457
= <=>
2025-10-15 16:21:25 +0700

    convert home,checkout, app to async await

--- e-commerce-project/src/pages/home/HomePage.jsx
+++ e-commerce-project/src/pages/home/HomePage.jsx
@@ -1,30 +1,31 @@
 import axios from 'axios';
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Header } from '../../components/Header.jsx'
 import './HomePage.css'
 import { ProductsGrid } from './ProductsGrid.jsx';
 
 
 
-export function HomePage({cart}) {
-  const [products,setProducts] = useState([]);
-  useEffect (()=> {
-    axios.get ('/api/products')
-      .then ((response)=> {
-        setProducts(response.data);
-      });
-  },[])
-  
+export function HomePage({ cart }) {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const getHomeData = async () => {
+      const response = await axios.get('/api/products');
+      setProducts(response.data);
+    };
+    getHomeData();
+  }, [])
+
 
   return (
     <>
 
       <title>Ecommerce Project</title>
       <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
-      <Header cart = {cart}/>
+      <Header cart={cart} />
       <div className="home-page">
-    
-        <ProductsGrid products = {products}/> 
+
+        <ProductsGrid products={products} />
       </div>
     </>
   );
